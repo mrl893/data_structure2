99602cccb033ff783735bac6f31aa70764f76b45
R morel <38894494+mrl893@users.noreply.github.com>
2023-08-29 00:34:40 -0400

    Update data_advanced_types.py

--- data_advanced_types.py
+++ data_advanced_types.py
@@ -1,2 +1,18 @@
 empty_list = []
 type(empty_list)
+
+empty_list = list() # constructor
+
+
+a_xpress = [2,3,5,7,11,13,17,19]
+fruits = ["Apple", "Mango", "Orange", "Kiwi", "Blueberry"]
+
+# class list:
+print(type(fruits)) 
+
+# access elements 
+# [1]
+# [-2]
+# [2:4]
+# [0::2]
+print(fruits[1])  == "mango" # mango True
