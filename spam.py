341bc6058fd9c709f2db502d3ed8b7338a6d4b86
R morel <38894494+mrl893@users.noreply.github.com>
2024-02-28 19:52:54 -0500

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,1 +1,26 @@
 
+import glob 
+import os
+
+
+e_mails = []
+labels = [] # [], []
+
+
+"""
+file_path = "mining/enron1/spam/0058.2003-12-21.GP.spam.txt" # ham/0007.1999-12-14.farmer.ham.txt"
+with open(file_path, "r") as infile:
+    # ham_sample 
+    spam_sample = infile.read()
+print(spam_sample)
+
+"""
+file_path = "mining/enron1/spam/"
+
+for filename in glob.glob(os.path.join(file_path, "*.txt")):
+    with open(filename, "r", encoding="ISO-8859-1") as infile:
+        e_mails.append(infile.read())
+        labels.append(0)
+print(len(e_mails))
+print(len(labels))
+    
